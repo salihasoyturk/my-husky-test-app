0847e0e19dda7a614f412647e10a46ed339c4426
salihasoyturk <soyturk.saliha@gmail.com>
2025-10-28 16:11:10 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+};
